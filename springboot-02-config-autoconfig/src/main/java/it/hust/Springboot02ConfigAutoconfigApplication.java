beba7a2da2a01feb10d0a4e4fa9bff529995c0cd
yaopeng <858728077@qq.com>
2019-10-02 17:03:31 +0800

    spring配置原理

---
+++ springboot-02-config-autoconfig/src/main/java/it/hust/Springboot02ConfigAutoconfigApplication.java
@@ -0,0 +1,14 @@
+package it.hust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02ConfigAutoconfigApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Springboot02ConfigAutoconfigApplication.class, args);
+	}
+
+}
