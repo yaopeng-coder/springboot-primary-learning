d810535a522a8e6ece5dae1e15700f01cdae4390
yaopeng <858728077@qq.com>
2019-10-05 16:09:55 +0800

    springboot自定义starter的测试实现

---
+++ springboot-08-test/src/main/java/cn/hsut/Springboot08TestApplication.java
@@ -0,0 +1,14 @@
+package cn.hsut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot08TestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Springboot08TestApplication.class, args);
+	}
+
+}
